48d38ac3ea897affc464333022b8251a10e35420
Eric Engelhart <bob80333@gmail.com>
2017-01-27 19:01:25 -0500

    Add Gradle-RIO and basic robot class

---
+++ src/main/java/org/usfirst/frc/team1251/Robot.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team1251;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * Created by Eric Engelhart on 1/27/2017.
+ */
+public class Robot extends IterativeRobot {
+}
