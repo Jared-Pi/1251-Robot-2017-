641d56559566cd1b735dc3df785820683aad750f
Jared <sparx030@users.noreply.github.com>
2017-01-27 19:08:29 -0500

    Added methods

--- src/main/java/org/usfirst/frc/team1251/Robot.java
+++ src/main/java/org/usfirst/frc/team1251/Robot.java
@@ -6,4 +6,16 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * Created by Eric Engelhart on 1/27/2017.
  */
 public class Robot extends IterativeRobot {
+    private static final int PWM_PORT_0 = 0;
+
+
+    public void robotInit() {
+
+    }
+    public void teleopInit() {
+
+    }
+    public void teleopPeriodic() {
+
+    }
 }
