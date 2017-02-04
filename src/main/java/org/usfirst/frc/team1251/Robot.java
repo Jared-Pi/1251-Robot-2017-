ae48cc107021222474f576dc2f0f18428ec5be28
Eric Engelhart <bob80333@gmail.com>
2017-02-03 19:23:02 -0500

    Testing pathfinder thingy

--- src/main/java/org/usfirst/frc/team1251/Robot.java
+++ src/main/java/org/usfirst/frc/team1251/Robot.java
@@ -6,4 +6,17 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * Created by Eric Engelhart on 1/27/2017.
  */
 public class Robot extends IterativeRobot {
+
+    public void robotInit(){
+
+    }
+
+    public void teleopInit(){
+
+    }
+
+    public void teleopPeriodic(){
+
+    }
+
 }
