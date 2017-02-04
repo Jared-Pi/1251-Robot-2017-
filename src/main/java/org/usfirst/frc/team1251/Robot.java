0128f9c5d7a513a258c15fdc972ccf40ef0e54b3
Eric Engelhart <bob80333@gmail.com>
2017-02-04 14:44:51 -0500

    Fixed it

--- src/main/java/org/usfirst/frc/team1251/Robot.java
+++ src/main/java/org/usfirst/frc/team1251/Robot.java
@@ -2,11 +2,11 @@ package org.usfirst.frc.team1251;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
+
 /**
  * Created by Eric Engelhart on 1/27/2017.
  */
 public class Robot extends IterativeRobot {
-
 
     public void robotInit(){
 
