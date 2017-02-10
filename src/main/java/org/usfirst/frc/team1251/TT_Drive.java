8fe792fec5e984b6fabfd318af69d09ad72c3a52
Jared <sparx030@users.noreply.github.com>
2017-02-10 18:40:10 -0500

    Drive base code

--- src/main/java/org/usfirst/frc/team1251/TT_Drive.java
+++ src/main/java/org/usfirst/frc/team1251/TT_Drive.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team1251;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 
@@ -7,8 +9,17 @@ import edu.wpi.first.wpilibj.RobotDrive;
  * Created by Eric Engelhart on 2/10/2017.
  */
 public class TT_Drive {
+    private static final DoubleSolenoid.Value HIGH_GEAR = DoubleSolenoid.Value.kForward;
+    private static final DoubleSolenoid.Value LOW_GEAR = DoubleSolenoid.Value.kReverse;
+
+    private static final int DRIVE_MULTIPLIER = 1;
+
+
     public static void drive(Joystick left, Joystick right, RobotDrive base){
-        base.tankDrive(left.getRawAxis(1), right.getRawAxis(1));
+        base.tankDrive(left.getRawAxis(1)*DRIVE_MULTIPLIER, right.getRawAxis(1)*DRIVE_MULTIPLIER);
     }
 
+    public static void shifter(Encoder left, Encoder right, DoubleSolenoid solenoid) {
+
+    }
 }
