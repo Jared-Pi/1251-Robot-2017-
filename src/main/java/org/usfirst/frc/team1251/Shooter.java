74cc935b04ea3c21741bf9308ed9a4f8e9d32cba
Nicholas Salazar <nicholas salazar>
2017-02-10 20:06:19 -0500

    Shooter class

---
+++ src/main/java/org/usfirst/frc/team1251/Shooter.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team1251;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+
+/**
+ * Created by Nicholas Salazar on 2/10/2017.
+ */
+
+public class Shooter {
+
+}
