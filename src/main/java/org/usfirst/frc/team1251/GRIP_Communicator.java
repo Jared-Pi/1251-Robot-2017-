3a287803ee4f6c5845a43073aed4a0f3949e274f
Eric Engelhart <bob80333@gmail.com>
2017-02-11 15:46:03 -0500

    add getAreaFromTable() method

--- src/main/java/org/usfirst/frc/team1251/GRIP_Communicator.java
+++ src/main/java/org/usfirst/frc/team1251/GRIP_Communicator.java
@@ -8,7 +8,17 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 public class GRIP_Communicator {
     private NetworkTable gripTable;
 
+    private double[] defaultValues;
+
     public GRIP_Communicator(NetworkTable gripTable){
         this.gripTable = gripTable;
+        defaultValues = new double[0];
     }
+
+    public double[] getAreaFromTable(String tableName){
+        double[] value;
+        value = gripTable.getSubTable(tableName).getNumberArray("area", defaultValues);
+        return value;
+    }
+
 }
