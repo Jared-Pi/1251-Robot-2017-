27f8af3c110fc40ceef2b435c95a2a145ce54d87
Eric Engelhart <bob80333@gmail.com>
2017-02-17 17:02:13 -0500

    weaken types to allow it to work for other inheriting classes

--- src/main/java/org/usfirst/frc/team1251/TT_Shooter.java
+++ src/main/java/org/usfirst/frc/team1251/TT_Shooter.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team1251;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.PIDInterface;
+import edu.wpi.first.wpilibj.SpeedController;
 
 /**
  * Created by Nicholas Salazar on 2/10/2017.
@@ -15,7 +15,7 @@ public class TT_Shooter {
 
     private static int agitationCounter = 0;
 
-    public static void shoot( Joystick joystick, Talon agitator, PIDController shooterPID) {
+    public static void shoot(Joystick joystick, SpeedController agitator, PIDInterface shooterPID) {
         if (joystick.getRawButton(1)) {
 
             shooterPID.setSetpoint(TT_Util.convertRPMsToTicks(SHOOTER_RPMS));
