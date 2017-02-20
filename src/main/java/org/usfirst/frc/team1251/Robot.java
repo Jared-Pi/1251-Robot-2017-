67aa4483dcac871911ecccce9c4c195c6cecf08e
Eric Engelhart <bob80333@gmail.com>
2017-02-20 14:28:57 -0500

    Create code to just test if every speedcontroller works, runs all motors at full speed

--- src/main/java/org/usfirst/frc/team1251/Robot.java
+++ src/main/java/org/usfirst/frc/team1251/Robot.java
@@ -1,47 +1,61 @@
 package org.usfirst.frc.team1251;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import jaci.pathfinder.Pathfinder;
-import jaci.pathfinder.Trajectory;
-import jaci.pathfinder.Waypoint;
-
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
 
 /**
  * Created by Eric Engelhart on 1/27/2017.
  */
 public class Robot extends IterativeRobot {
-    // 3 Waypoints
-    Waypoint[] points = new Waypoint[] {
-            new Waypoint(-4, -1, Pathfinder.d2r(-45)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
-            new Waypoint(-2, -2, 0),                        // Waypoint @ x=-2, y=-2, exit angle=0 radians
-            new Waypoint(0, 0, 0)                           //  Waypoint @ x=0, y=0,   exit angle=0 radians
-    };
+    // only joystick button
+    private static final int LOGITECH_A_BUTTON = 2;
 
-    // Create the Trajectory Configuration
-//
-// Arguments:
-// Fit Method:          HERMITE_CUBIC or HERMITE_QUINTIC
-// Sample Count:        SAMPLES_HIGH (100 000)
-//                      SAMPLES_LOW  (10 000)
-//                      SAMPLES_FAST (1 000)
-// Time Step:           0.05 Seconds
-// Max Velocity:        1.7 m/s
-// Max Acceleration:    2.0 m/s/s
-// Max Jerk:            60.0 m/s/s/s
-    Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC,
-            Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0);
+    // joystick
+    private Joystick logitech;
 
-    // Generate the trajectory
-    Trajectory trajectory = Pathfinder.generate(points, config);
+    //Define PWM ports
+    private static final int PWM_PORT_0 = 0;
+    private static final int PWM_PORT_1 = 1;
+    private static final int PWM_PORT_2 = 2;
+    private static final int PWM_PORT_3 = 3;
+    private static final int PWM_PORT_4 = 4;
+    private static final int PWM_PORT_5 = 5;
+    private static final int PWM_PORT_6 = 6;
+    private static final int PWM_PORT_7 = 7;
+    private static final int PWM_PORT_8 = 8;
+    private static final int PWM_PORT_9 = 9;
 
-    int trajectoryIndex = 0;
+    // create talons
+    private Talon talon1;
+    private Talon talon2;
+    private Talon talon3;
+    private Talon talon4;
+    private Talon talon5;
+    private Talon talon6;
+    private Talon talon7;
+    private Talon talon8;
+    private Talon talon9;
+    private Talon talon10;
+
     public void robotInit(){
+        talon1 = new Talon(PWM_PORT_0);
+        talon2 = new Talon(PWM_PORT_1);
+        talon3 = new Talon(PWM_PORT_2);
+        talon4 = new Talon(PWM_PORT_3);
+        talon5 = new Talon(PWM_PORT_4);
+        talon6 = new Talon(PWM_PORT_5);
+        talon7 = new Talon(PWM_PORT_6);
+        talon8 = new Talon(PWM_PORT_7);
+        talon9 = new Talon(PWM_PORT_8);
+        talon10 = new Talon(PWM_PORT_9);
+
+        logitech = new Joystick(0);
 
     }
 
     public void autonomousInit(){
-        Trajectory.Segment currentSegment = trajectory.get(trajectoryIndex);
-        trajectoryIndex++;
+
     }
 
     public void teleopInit(){
@@ -49,8 +63,29 @@ public class Robot extends IterativeRobot {
     }
 
     public void teleopPeriodic(){
-
-
+        if (logitech.getRawButton(LOGITECH_A_BUTTON)) {
+            talon1.set(1);
+            talon2.set(1);
+            talon3.set(1);
+            talon4.set(1);
+            talon5.set(1);
+            talon6.set(1);
+            talon7.set(1);
+            talon8.set(1);
+            talon9.set(1);
+            talon10.set(1);
+        } else {
+            talon1.set(0);
+            talon2.set(0);
+            talon3.set(0);
+            talon4.set(0);
+            talon5.set(0);
+            talon6.set(0);
+            talon7.set(0);
+            talon8.set(0);
+            talon9.set(0);
+            talon10.set(0);
+        }
 
     }
 
