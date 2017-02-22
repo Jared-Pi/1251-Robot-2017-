4f27f67f06cd4a96ef11dd1d9db064191def6877
Nicholas Salazar <nicholas salazar>
2017-02-21 21:42:43 -0500

    Added starting methods to GearCollector and Added code within the collectGearFloor method.

--- src/main/java/org/usfirst/frc/team1251/TT_GearCollector.java
+++ src/main/java/org/usfirst/frc/team1251/TT_GearCollector.java
@@ -6,10 +6,28 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 
 /**
- * Created by Jared on 2/11/2017.
+ * Created by Jared on 2/11/2017.  Edited by Nicholas Salazar on 2/21/17
  */
-public class TT_GearCollector {
-    public static void collectGear(Joystick controller, Talon motor, DoubleSolenoid pivot, DoubleSolenoid claw, DigitalInput sensor) {
 
+
+public class TT_GearCollector {
+    //First attempt
+    public static void collectGearFloor(Joystick controller, Talon motor, DoubleSolenoid pivot, DoubleSolenoid claw, DigitalInput sensor) {
+        if (controller.getRawButton(7)) { //Check how the controller is utilised, e.g. button or axis; This will be for from floor
+            pivot.set(DoubleSolenoid.Value.kForward);
+            claw.set(DoubleSolenoid.Value.kForward);
+            while (!sensor.get()) {
+                motor.set(-1);
+            }
+            claw.set(DoubleSolenoid.Value.kReverse);
+            pivot.set(DoubleSolenoid.Value.kReverse);
+        }
     }
+
+    public static void collectGearPlayer(Joystick controller, Talon motor, DoubleSolenoid pivot, DoubleSolenoid claw, DigitalInput sensor) {
+        if (controller.getRawButton(9)) {
+
+        }
+    }
+
 }
