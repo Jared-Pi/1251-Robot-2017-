649cdc4a8804ae5ab7a23ade5090685181750ce6
Eric Engelhart <bob80333@gmail.com>
2017-02-26 11:54:31 -0500

    Different encoders have different ticks
    That 1024 would mess up the drivetrain/shooter encoders afaik

--- src/main/java/org/usfirst/frc/team1251/TT_Util.java
+++ src/main/java/org/usfirst/frc/team1251/TT_Util.java
@@ -5,14 +5,21 @@ package org.usfirst.frc.team1251;
  */
 public class TT_Util {
 
-    public static double convertTicksToRPMs(double speed){
+    public static double convertTicksToRPMsHangar(double speed){
         // divide by ticks per revolution
         speed /= 1024;
         // divide by seconds per minute
         speed /= 60;
         return speed;
     }
-
+    
+    public static double convertTicksToRPMs(double speed){
+        // divide by ticks per revolution
+        speed /= 125;
+        // divide by seconds per minute
+        speed /= 60;
+        return speed;
+    }
     public static double convertRPMsToTicks(double speed){
         // multiply by ticks per revolution
         speed *= 125;
