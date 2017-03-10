e1d9761ba763dda0dad578a19826227b969b282e
Eric Engelhart <bob80333@gmail.com>
2017-03-09 22:09:50 -0500

    Add rotateBot method and fix driveStraight

--- src/main/java/org/usfirst/frc/team1251/TT_Util.java
+++ src/main/java/org/usfirst/frc/team1251/TT_Util.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1251;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -9,8 +10,15 @@ import edu.wpi.first.wpilibj.RobotDrive;
  */
 public class TT_Util {
 
-    static double lSpeed = 0;
-    static double rSpeed = 0;
+    static double lDriveSpeed = 0;
+    static double rDriveSpeed = 0;
+    static boolean firstDrive = true;
+
+    static double lRotateSpeed = 0;
+    static double rRotateSpeed = 0;
+    static double startAngle = 0;
+    static double endAngle = 0;
+    static boolean firstRotate = true;
 
     public static double convertTicksToRPMsHanger(double speed){
         // divide by ticks per revolution
@@ -35,28 +43,57 @@ public class TT_Util {
         return  speed;
     }
 
-    public static void driveStraight(RobotDrive baseDrive, DoubleSolenoid baseShifter, Encoder lEncoder, Encoder rEncoder){
+    // return 1 if still driving, return 0 if done
+    public static int driveStraight(RobotDrive baseDrive, DoubleSolenoid baseShifter, Encoder lEncoder, Encoder rEncoder, double distance){
+        if (firstDrive){
+            lEncoder.reset();
+            rEncoder.reset();
+            firstDrive = false;
+        }
+
         if (lEncoder.getRate()<1.2) {
-            lSpeed -= 0.01;
+            lDriveSpeed -= 0.01;
         }
         else if (lEncoder.getRate()>1.3) {
-            lSpeed += 0.01;
+            lDriveSpeed += 0.01;
         }
 
         if (rEncoder.getRate()<1.2) {
-            rSpeed -= 0.01;
+            rDriveSpeed -= 0.01;
         }
         else if (rEncoder.getRate()>1.3) {
-            rSpeed += 0.01;
+            rDriveSpeed += 0.01;
         }
 
         baseShifter.set(DoubleSolenoid.Value.kReverse);
-        if (lEncoder.getDistance() <= 4 && lEncoder.getDistance() <= 4) {
-            baseDrive.tankDrive(lSpeed,rSpeed);
+        if (lEncoder.getDistance() <= distance && lEncoder.getDistance() <= distance) {
+            baseDrive.tankDrive(lDriveSpeed, rDriveSpeed);
+            return 1;
         }
         else {
-            baseDrive.tankDrive(0, 0);
+            firstDrive = true;
+            return 0;
         }
+    }
+
+    public static int rotateBot(double angle, RobotDrive baseDrive, ADXRS450_Gyro gyro){
+        if (firstRotate){
+            startAngle = gyro.getAngle();
+            endAngle = gyro.getAngle() + angle;
+            firstRotate = false;
+        }
+
+        lRotateSpeed = -(Math.abs(gyro.getAngle() - endAngle) * (1.0 / angle)); // get difference between now and end and multiply by 1/ total degrees to travel
+        rRotateSpeed = Math.abs(gyro.getAngle() - endAngle) * (1.0 / angle);
+
+        if (!(Math.abs(gyro.getAngle()) < Math.abs(endAngle) + 1 && Math.abs(gyro.getAngle()) > Math.abs(endAngle) - 1)) {
+            baseDrive.tankDrive(lRotateSpeed, rRotateSpeed);
+            return 1;
+        } else {
+            firstRotate = true;
+            return 0;
+        }
+
     }
 
 }
