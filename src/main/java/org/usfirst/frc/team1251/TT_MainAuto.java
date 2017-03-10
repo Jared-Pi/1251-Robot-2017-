3974c7c3275fc1d8eebfc3964d6c5b0c94c0c69c
Eric Engelhart <bob80333@gmail.com>
2017-03-09 21:49:20 -0500

    Make driveStraight into Util

--- src/main/java/org/usfirst/frc/team1251/TT_MainAuto.java
+++ src/main/java/org/usfirst/frc/team1251/TT_MainAuto.java
@@ -10,9 +10,6 @@ import edu.wpi.first.wpilibj.interfaces.Gyro;
  * Created by Jared on 3/9/2017.
  */
 public class TT_MainAuto {
-    static int autoCounter = 0;
-    static double lSpeed = 0;
-    static double rSpeed = 0;
 
     public static void auto(int autoSelect, RobotDrive baseDrive, DoubleSolenoid baseShifter, SpeedController pivotMotor, DoubleSolenoid claw, Gyro gyro, Encoder lEncoder, Encoder rEncoder) {
         switch(autoSelect){
@@ -22,28 +19,6 @@ public class TT_MainAuto {
 
             case 1:
 
-                if (lEncoder.getRate()<1.2) {
-                    lSpeed -= 0.01;
-                }
-                else if (lEncoder.getRate()>1.3) {
-                    lSpeed += 0.01;
-                }
-
-                if (rEncoder.getRate()<1.2) {
-                    rSpeed -= 0.01;
-                }
-                else if (rEncoder.getRate()>1.3) {
-                    rSpeed += 0.01;
-                }
-
-                baseShifter.set(DoubleSolenoid.Value.kReverse);
-                if (lEncoder.getDistance() <= 4 && lEncoder.getDistance() <= 4) {
-                    baseDrive.tankDrive(lSpeed,rSpeed);
-                }
-                else {
-                    baseDrive.tankDrive(0, 0);
-                }
-                autoCounter++;
                 break;
             case 2:
                 claw.set(DoubleSolenoid.Value.kReverse);
--- src/main/java/org/usfirst/frc/team1251/TT_Util.java
+++ src/main/java/org/usfirst/frc/team1251/TT_Util.java
@@ -1,9 +1,16 @@
 package org.usfirst.frc.team1251;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotDrive;
+
 /**
  * Created by Jared on 2/10/2017.
  */
 public class TT_Util {
+
+    static double lSpeed = 0;
+    static double rSpeed = 0;
 
     public static double convertTicksToRPMsHanger(double speed){
         // divide by ticks per revolution
@@ -26,6 +33,30 @@ public class TT_Util {
         // multiply by seconds per minute
         speed *= 60;
         return  speed;
+    }
+
+    public static void driveStraight(RobotDrive baseDrive, DoubleSolenoid baseShifter, Encoder lEncoder, Encoder rEncoder){
+        if (lEncoder.getRate()<1.2) {
+            lSpeed -= 0.01;
+        }
+        else if (lEncoder.getRate()>1.3) {
+            lSpeed += 0.01;
+        }
+
+        if (rEncoder.getRate()<1.2) {
+            rSpeed -= 0.01;
+        }
+        else if (rEncoder.getRate()>1.3) {
+            rSpeed += 0.01;
+        }
+
+        baseShifter.set(DoubleSolenoid.Value.kReverse);
+        if (lEncoder.getDistance() <= 4 && lEncoder.getDistance() <= 4) {
+            baseDrive.tankDrive(lSpeed,rSpeed);
+        }
+        else {
+            baseDrive.tankDrive(0, 0);
+        }
     }
 
 }
