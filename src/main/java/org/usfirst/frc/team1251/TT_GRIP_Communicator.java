4456157f7480a56004fe4861f3f46f2ded227303
Eric Engelhart <bob80333@gmail.com>
2017-03-15 21:17:35 -0400

    Try gear tracking stuff a bit

--- src/main/java/org/usfirst/frc/team1251/TT_GRIP_Communicator.java
+++ src/main/java/org/usfirst/frc/team1251/TT_GRIP_Communicator.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
  * Created by Eric Engelhart on 2/11/2017.
  */
 public class TT_GRIP_Communicator {
+    public static TT_GRIP_Communicator INSTANCE;
     private NetworkTable gripTable;
 
     private double[] defaultValues;
@@ -13,11 +14,18 @@ public class TT_GRIP_Communicator {
     public TT_GRIP_Communicator(NetworkTable gripTable){
         this.gripTable = gripTable;
         defaultValues = new double[0];
+        INSTANCE = this;
     }
 
     public double[] getAreaFromTable(String tableName){
         double[] value;
         value = gripTable.getSubTable(tableName).getNumberArray("area", defaultValues);
+        return value;
+    }
+
+    public double[] getXFromTable(String tableName) {
+        double[] value;
+        value = gripTable.getSubTable(tableName).getNumberArray("x", defaultValues);
         return value;
     }
 
--- src/main/java/org/usfirst/frc/team1251/TT_GearTracker.java
+++ src/main/java/org/usfirst/frc/team1251/TT_GearTracker.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team1251;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  * Created by Eric Engelhart on 2/10/2017.
  */
@@ -10,7 +12,15 @@ public class TT_GearTracker {
     }
 
     public boolean track(){
-        // ez vision tracking
+        double[] tape = TT_GRIP_Communicator.INSTANCE.getAreaFromTable("Gear");
+        if (tape.length < 2 || tape.length > 2) {
+            SmartDashboard.putBoolean("Gear Tracking", false);
+            return false;
+        }
+        double[] Xs = TT_GRIP_Communicator.INSTANCE.getXFromTable("Gear");
+        if (Xs[0] > Xs[1]) {
+
+        }
         return false;
     }
 
